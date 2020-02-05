6a9a91a0db34c4c40d11deb7801466b34375b6b5
Florencia Gomez <Florxgomez@gmail.com>
2020-02-05 18:53:27 -0300

    Create index.ts

---
+++ index.ts
@@ -0,0 +1,33 @@
+//instructions to every other class on how they can be an argument to 'addMarker'
+interface Mappable {
+ location: {
+     lat: number;
+     lng: number
+ }
+}
+
+export class CustomMap {
+    private googleMap: google.maps.Map;
+
+    constructor(divId: string) {
+        this.googleMap = new google.maps.Map(document.querySelector(divId), {
+            backgroundColor: 'pink',
+            zoom: 1, center: {
+                lat: 0,
+                lng: 0
+            }
+        });
+    }
+
+    addMarker(mappable: Mappable): void {
+        new google.maps.Marker({
+            map: this.googleMap,
+            position: {
+                lat: mappable.location.lat,
+                lng: mappable.location.lng
+            }
+        })
+    }
+}
+
+
