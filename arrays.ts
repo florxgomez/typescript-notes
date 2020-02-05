c2257f80399932d7071c23602db21fa6b63810f3
Florencia Gomez <Florxgomez@gmail.com>
2020-02-05 11:13:41 -0300

    Create arrays.ts

---
+++ arrays.ts
@@ -0,0 +1,23 @@
+const carMakers: string[] = ['ford', 'toyota', 'chevy'];
+
+
+const carsByMake: string[][] = [
+    ['f150'],
+    ['corolla'],
+    ['camaro']
+]
+
+//Help with inference when extracting values
+const car = carMakers [0];
+const myCar = carMakers.pop();
+
+//Preventing incompatible values
+carMakers.push(100); //error!
+
+//help with map
+carMakers.map((car: string): string => {
+    return car.toUpperCase();
+})
+
+//Flexible types
+const importantDates: (Date | string)[] = [new Date(), '2030-10-10'];
