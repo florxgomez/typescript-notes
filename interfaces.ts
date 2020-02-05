f7873215d1a0f0f58a3e8281d145124417119b37
Florencia Gomez <Florxgomez@gmail.com>
2020-02-05 11:13:03 -0300

    Create interfaces.ts

---
+++ interfaces.ts
@@ -0,0 +1,29 @@
+interface Reportable {
+    summary(): string;
+}
+
+const oldCivic = {
+    name: 'civic',
+    year: 2000,
+    broken: true,
+    summary(): string {
+        return `Name: ${this.name}`
+    }
+};
+
+const drink = {
+    color: 'brown',
+    carbonated: true,
+    sugar: 40,
+    summary(): string {
+        return `My drink has ${this.sugar} grams of sugar`
+    }
+}
+
+//This function can be used with any object that satisfies the Reportable interface
+const printSummary = (item: Reportable): void => {
+    console.log(item.summary());
+};
+
+printSummary(oldCivic);
+printSummary(drink);
