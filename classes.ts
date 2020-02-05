7fc4a5d6ec86a889709776907ba3cbdb3870c1d3
Florencia Gomez <Florxgomez@gmail.com>
2020-02-05 12:09:50 -0300

    Update classes.ts

--- classes.ts
+++ classes.ts
@@ -1,21 +1,28 @@
 class Vehicle {
-    drive(): void{
-        console.log('chugga chugga');
-    }
-    honk(): void {
+    constructor(public color: string){}
+
+    protected honk(): void {
         console.log('beep');
     }
 }
 
 class Car extends Vehicle { //Vehice is the superclass (basic inheritance)
-    drive(): void{
+    constructor(public wheels: number, color: string){
+        super(color);
+    }
+
+    private drive(): void{
         console.log('vroom');
+    }
+
+    public startDrivingProcess(): void {
+        this.drive();
+        this.honk();
     }
 }
 
-const vehicle = new Vehicle();
-vehicle.drive();
-vehicle.honk();
+const vehicle = new Vehicle('orange');
+console.log(vehicle.color);
 
-const car = new Car();
-car.drive();
+const car = new Car(4,'red');
+car.startDrivingProcess();
